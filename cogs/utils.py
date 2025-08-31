8ce3ce45d4da77e9f87b97fcaa9ee25deb3038ab
OpAdarsh <80208864+OpAdarsh@users.noreply.github.com>
2025-08-31 10:25:11 +0530

    Update utils.py

--- cogs/utils.py
+++ cogs/utils.py
@@ -36,71 +36,8 @@ class Utils(commands.Cog):
         except (SyntaxError, ZeroDivisionError, TypeError, OverflowError) as e:
             await ctx.send(f"Invalid mathematical expression: `{e}`")
 
-    @commands.command(name='slots', help="!slots <bet> - Play the slot machine!")
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def slots(self, ctx, bet: int):
-        """A weighted slot machine game with a cleaner UI."""
-        player = db.get_player(ctx.author.id)
+    
         
-        if bet <= 0:
-            await ctx.send("You must bet a positive amount of coins."); return
-        if player['coins'] < bet:
-            await ctx.send("You don't have enough coins to make that bet."); return
-
-        player['coins'] -= bet
-        
-        reels = {'🍒': 25, '💰': 20, '🔔': 15, '💎': 5}
-        
-        spinning_embed = discord.Embed(title="🎰 Slot Machine 🎰", description="Spinning the reels...", color=discord.Color.blue())
-        spinning_embed.add_field(name="Bet", value=f"{bet} coins")
-        msg = await ctx.send(embed=spinning_embed)
-        await asyncio.sleep(2)
-
-        result_reels = random.choices(list(reels.keys()), weights=list(reels.values()), k=3)
-        
-        winnings = 0
-        result_text = ""
-        win_color = discord.Color.red()
-        
-        if result_reels.count('💎') == 3:
-            winnings = bet * 10
-            result_text = f"💎💎💎 **JACKPOT!** You win **{winnings}** coins! 💎💎💎"
-            win_color = discord.Color.gold()
-        elif result_reels.count('🍒') == 3:
-            winnings = bet * 5
-            result_text = f"🍒🍒🍒 **BIG WIN!** You get **{winnings}** coins! 🍒🍒🍒"
-            win_color = discord.Color.green()
-        elif result_reels.count('💰') == 3:
-            winnings = bet * 2
-            result_text = f"💰💰💰 **You doubled up!** You win **{winnings}** coins! 💰💰💰"
-            win_color = discord.Color.green()
-        elif result_reels.count('🔔') == 3:
-            winnings = bet
-            result_text = f"🔔🔔🔔 **Bet back!** You get **{winnings}** coins! 🔔🔔🔔"
-            win_color = discord.Color.light_grey()
-        else:
-            result_text = "Sorry, you lost this time. Better luck next time!"
-            
-        player['coins'] += winnings
-        db.update_player(ctx.author.id, player)
-
-        final_embed = discord.Embed(title="🎰 Slot Machine 🎰", description=result_text, color=win_color)
-        final_embed.add_field(
-            name="Result", 
-            value=f"**` {result_reels[0]} | {result_reels[1]} | {result_reels[2]} `**", 
-            inline=False
-        )
-        final_embed.set_footer(text=f"Your new balance: {player['coins']} coins")
-        await msg.edit(embed=final_embed)
-
-    @slots.error
-    async def slots_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Please specify an amount to bet. Usage: `!slots <amount>`")
-        elif isinstance(error, commands.BadArgument):
-            await ctx.send("Please enter a valid number for your bet.")
-        elif isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(f"The slot machine is cooling down! Try again in {error.retry_after:.1f} seconds.", delete_after=5)
 
 async def setup(bot):
     await bot.add_cog(Utils(bot))
